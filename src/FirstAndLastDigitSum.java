66cb63478613baa73e57f1c36dd07af9c100955e
WilliamCarvalho <williamcarvalh0@hotmail.com>
2020-04-07 10:23:27 +0100

    Initial commit

---
+++ src/FirstAndLastDigitSum.java
@@ -0,0 +1,38 @@
+//       Write a method named sumFirstAndLastDigit with one parameter of type int called number.
+//       The method needs to find the first and the last digit of the parameter number passed to the method, using a loop and return the sum of the first and the last digit of that number.
+//       If the number is negative then the method needs to return -1 to indicate an invalid value.
+//
+//        Example input/output
+//       * sumFirstAndLastDigit(252); → should return 4, the first digit is 2 and the last is 2 which gives us 2+2 and the sum is 4.
+//       * sumFirstAndLastDigit(257); → should return 9, the first digit is 2 and the last is 7 which gives us 2+7 and the sum is 9.
+//       * sumFirstAndLastDigit(0); → should return 0, the first digit and the last digit is 0 since we only have 1 digit, which gives us 0+0 and the sum is 0.
+//       * sumFirstAndLastDigit(5); → should return 10, the first digit and the last digit is 5 since we only have 1 digit, which gives us 5+5 and the sum is 10.
+//       * sumFirstAndLastDigit(-10); → should return -1, since the parameter is negative and needs to be positive.
+//
+//        NOTE: The method sumFirstAndLastDigit needs to be defined as public static like we have been doing so far in the course.
+
+public class FirstAndLastDigitSum {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));// 2 + 2 = 4
+        System.out.println(sumFirstAndLastDigit(257));// 2 + 7 = 9
+        System.out.println(sumFirstAndLastDigit(0));// 0
+        System.out.println(sumFirstAndLastDigit(5));// 5 + 5 = 10
+        System.out.println(sumFirstAndLastDigit(-10));// -1
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        int sum = 0;
+        int lastDigit = 0;
+        if (number >= 0) {
+            sum += number % 10;
+            while (number != 0) {
+                lastDigit = number % 10;
+                number /= 10;
+            }
+            sum += lastDigit;
+            return sum;
+        } else {
+            return -1;
+        }
+    }
+}
